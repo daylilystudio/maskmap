32d67da2edc68d2d61a95cab01f005526609d4c6
Daylily <daylily@mac>
2023-07-14 16:58:42 +0800

    edit info

--- src/components/infoModal.vue
+++ src/components/infoModal.vue
@@ -5,24 +5,24 @@
   <transition>
     <div v-show="ifOpenInfo" class="z-20 fixed w-full h-full flex justify-center items-center">
       <div class="bg-white text-gray-title shadow rounded-2xl relative z-50 pt-4 pb-5">
-        <h5 class="px-8 text-xl font-medium text-center md:text-left">口罩數量顏色提示</h5>
+        <h5 class="px-8 text-xl font-medium text-center md:text-left">The Color Tips of Mask Number</h5>
         <hr class="w-full h-px my-4 bg-gray-decolight border-0">
         <ul class="flex flex-col md:flex-row px-8">
           <li class="flex justify-center items-center mb-4 md:mb-0 md:mr-8">
-            <span class="inline-block w-16 h-8 mr-2 rounded-full bg-enough-color text-xs text-white flex justify-center items-center">10 ↑</span>充 足
+            <span class="inline-block w-16 h-8 mr-2 rounded-full bg-enough-color text-xs text-white flex justify-center items-center">10 ↑</span>Sufficient
           </li>
           <li class="flex justify-center items-center mb-4 md:mb-0 md:mr-8">
-            <span class="inline-block w-16 h-8 mr-2 rounded-full bg-little-color text-xs text-white flex justify-center items-center">10 ↓</span>少 量
+            <span class="inline-block w-16 h-8 mr-2 rounded-full bg-little-color text-xs text-white flex justify-center items-center">10 ↓</span>Less
           </li>
           <li class="flex justify-center items-center">
-            <span class="inline-block w-16 h-8 mr-2 rounded-full bg-gray-tips text-xs text-white flex justify-center items-center">0</span>缺 貨
+            <span class="inline-block w-16 h-8 mr-2 rounded-full bg-gray-tips text-xs text-white flex justify-center items-center">0</span>Run out
           </li>
         </ul>
         <p class="text-sm text-gray-tips text-center mt-5 mb-4 px-4">
-          ※ 地圖標記點顏色以成人為主<br>
-          ※ 點左側清單藥局名可移至地圖標記點<br><br>
-          庫存量最後更新時間 <br class="sm:hidden">{{mapData[0].properties.updated}}<br>
-          防疫專線 1922 ｜ 口罩資訊 1919
+          ※ The color depends on the mask quantity of adult<br>
+          ※ Clicking the Pharmacy name can move to the map point<br><br>
+          The stock last updated : <br class="sm:hidden">{{mapData[0].properties.updated}}<br>
+          Hotline 1922 ｜ Mask Info 1919
         </p>
         <p class="text-sm font-medium text-center px-4">
           This site serves for Taiwan<br>
